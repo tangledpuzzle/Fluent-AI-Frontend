d5ddc2f0a35e0cfeb38310621c7600523a6686ae
tangledpuzzle <tanglepduzzle94621@gmail.com>
2024-10-03 03:51:09 -0700

    feat: upgrade demo_video

--- fluentai-backend/app/routes/__init__.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-from . import user, chat,languages,referrals
-
-router = APIRouter()
-
-router.include_router(user.router, prefix="/user")
-router.include_router(languages.router, prefix="/languages")
-router.include_router(referrals.router, prefix="/referrals")
-router.include_router(chat.router, prefix="/chat")
